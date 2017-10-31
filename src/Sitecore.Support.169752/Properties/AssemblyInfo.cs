1e6a8269cc9a6d650375a1347d775cb0b166a728
Kirill Volovitskiy <kiv@sitecore.net>
2017-10-31 16:00:04 +0200

    default code

---
+++ src/Sitecore.Support.169752/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.169752")]
+[assembly: AssemblyProduct("Sitecore.Support.169752")]
+[assembly: ComVisible(false)]
